c9a8b04687a945cb9416eed193a320def5249fe8
tankhust <347477499@qq.com>
2019-01-14 21:18:50 +0800

    基础微服务-条件查询

--- tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
+++ tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
@@ -3,10 +3,19 @@ package com.tensquare.base.service;
 import com.tensquare.base.dao.LabelDao;
 import com.tensquare.base.pojo.Label;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import util.IdWorker;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -41,5 +50,65 @@ public class LabelService {
 
     public void deleteById(String id) {
         labelDao.deleteById(id);
+    }
+
+    public List<Label> findSearch(Label label) {
+        return labelDao.findAll(new Specification<Label>(){
+            /**
+             * @param root 根对象，也就是要把条件封装到哪个对象中，where 类名=label.getId
+             * @param query 封装的都是查询关键字，比如group by  order by 等
+             * @param cb 用来封装条件对象的，如果直接返回null，表示不需要任何条件
+             * @return
+             */
+            @Override
+            public Predicate toPredicate(Root<Label> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                //new一个集合，来存放所有的条件
+                List<Predicate> list=new ArrayList<>();
+                if (label.getLabelname() != null && "" != label.getLabelname()) {
+                    Predicate predicate = cb.like(root.get("labelname").as(String.class), "%" + label.getLabelname() + "%");//labelname like "%小明%"
+                    list.add(predicate);
+                }
+                if (label.getState() != null && "" != label.getState()) {
+                    Predicate predicate = cb.equal(root.get("state").as(String.class), label.getState());//getState="1"
+                    list.add(predicate);
+                }
+                //new一个数组作为最终返回值的条件
+                Predicate[] pred = new Predicate[list.size()];
+                //把list直接转成数组
+                pred=list.toArray(pred);
+                return cb.and(pred);//where labelname like "%小明%" and getState="1"
+            }
+        });
+    }
+
+    public Page<Label> pageQuery(Label label, int page, int size) {
+        //封装分页对象
+        Pageable pageable= PageRequest.of(page-1, size);
+        return labelDao.findAll(new Specification<Label>(){
+            /**
+             * @param root 根对象，也就是要把条件封装到哪个对象中，where 类名=label.getId
+             * @param query 封装的都是查询关键字，比如group by  order by 等
+             * @param cb 用来封装条件对象的，如果直接返回null，表示不需要任何条件
+             * @return
+             */
+            @Override
+            public Predicate toPredicate(Root<Label> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                //new一个集合，来存放所有的条件
+                List<Predicate> list=new ArrayList<>();
+                if (label.getLabelname() != null && "" != label.getLabelname()) {
+                    Predicate predicate = cb.like(root.get("labelname").as(String.class), "%" + label.getLabelname() + "%");//labelname like "%小明%"
+                    list.add(predicate);
+                }
+                if (label.getState() != null && "" != label.getState()) {
+                    Predicate predicate = cb.equal(root.get("state").as(String.class), label.getState());//getState="1"
+                    list.add(predicate);
+                }
+                //new一个数组作为最终返回值的条件
+                Predicate[] pred = new Predicate[list.size()];
+                //把list直接转成数组
+                pred=list.toArray(pred);
+                return cb.and(pred);//where labelname like "%小明%" and getState="1"
+            }
+        }, pageable);
     }
 }
