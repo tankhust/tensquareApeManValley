30a8b78972d5c34066d244a9723bd34fc48fc25b
tankhust <347477499@qq.com>
2019-01-15 17:30:36 +0800

    使用SpringDataMongoDB完成吐槽微服务的开发

---
+++ tensquare_spit/src/main/java/com/tensquare/spit/SpitApplication.java
@@ -0,0 +1,22 @@
+package com.tensquare.spit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import util.IdWorker;
+
+/**
+ * @author tank
+ * @create 2019/01/15 15:24
+ */
+@SpringBootApplication
+public class SpitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpitApplication.class);
+    }
+
+    @Bean
+    public IdWorker idWorker() {
+        return new IdWorker(1,1);
+    }
+}
